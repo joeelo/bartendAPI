76630b4614277452dfc9315c4dde35def390c15f
Gracie <m.graciemcguire@gmail.com>
2020-02-20 10:48:25 -0500

    zs

---
+++ data/gData.js
@@ -0,0 +1,45 @@
+const drinks =
+  {
+      "name": "Zombie Sleepwalker",
+      "ingredients": {
+        'dark rum': '1 ounce',
+        'light rum': '1 ounce',
+        'Jamaican rum': '1 ounce',
+        'cherry brandy': '4 dashes',
+        'apricot brandy': '4 dashes',
+        'papaya juice': '1 dash',
+        '150-proof rum': '.5 ounce'
+      },
+      "instructions": "1. Pour all rums, brandies, papaya, and lime juices
+        in a zombie glass with cracked ice.
+        2. Top with 150-proof rum.
+        3. Stir.
+        4. Garnish with a cherry and an orange slice.
+        5. Serve with a long straw.",
+      "image": '',
+      "description": "Legend has it that naval men would drink these and then
+      proceed to stumble to the plank after lights out. The result:
+      honorable discharge."
+  },
+  {
+      "name": "Zombie",
+      "ingredients": {
+        'Unsweetened Pineapple Juice': '1 ounce',
+        'lime juice': "one lime's worth",
+        'lime juice': "one small orange's worth",
+        'powdered sugar': '1 teaspoon',
+        'apricot brandy': '.5 ounce',
+        'passion fruit syrup': '1 ounce',
+        '150-proof rum': '1 ounce',
+        'crushed ice': '.5 cup'
+      },
+      "instructions": "1. Pour all juices, powdered sugar, apricot brandy,
+      rums, and passion fruit syrup in a blender.
+      2. Blend at low speed for 1 minute.
+      3. Strain into a frosted highball glass.
+      4. Garnish with a pineapple stick and a cherry.
+      5. Float 151-proof rum on top",
+      "image": '',
+      "description": "Let your eyes roll back in your head and get ready for some
+      voodoo loving. Only one per customer, please."
+  },
