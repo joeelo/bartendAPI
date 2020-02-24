bc949eaf1c622e5b0305fd604b2a74229d611fa7
Gracie <m.graciemcguire@gmail.com>
2020-02-24 17:41:58 -0500

    up to whiskey sour. woof

--- data/gData.js
+++ data/gData.js
@@ -187,31 +187,255 @@ const drinks =
         2. Add Strega, crème de menthe, and juices.
         3. Shake.
         4. Strain into a collins glass with shaved ice.
-        5. Float three or four drops Pernod.
+        5. Float three or four drops of Pernod.
         6. Decorate with fresh mint.",
       "image": '',
       "description": "The popular year-around drink in Salem. Bottoms up, Esmeralda."
     },
     {
-        "name": "Winter Sparkler",
-        "ingredients": {
-          'lemon juice': '1 lemon',
-          'lime juice': '1 lime',
-          'Southern Comfort': '8 ounces',
-          'peach schnapps': '8 ounces',
-          'brut champagne': '2 fifths',
-          'peaches': '3 peaches',
-          'whole cloves': '30',
-          'ice block': '1'
-        },
-        "instructions": "1. Place ice in punch bowl.
-        2. Add juices.
-        3. Add peach schnapps and Southern Comfort.
-        4. Stir.
-        5. Stud peach skins with cloves and add to bowl.
-        6. Add champagne.
-        7. Stir gently",
-        "image": '',
-        "description": "A bubbly concoction designed to lighten up any family
-        festivity."
+      "name": "Winter Sparkler",
+      "ingredients": {
+        'lemon juice': '1 lemon',
+        'lime juice': '1 lime',
+        'Southern Comfort': '8 ounces',
+        'peach schnapps': '8 ounces',
+        'brut champagne': '2 fifths',
+        'peaches': '3 peaches',
+        'whole cloves': '30',
+        'ice block': '1'
+      },
+      "instructions": "1. Place ice in punch bowl.
+      2. Add juices.
+      3. Add peach schnapps and Southern Comfort.
+      4. Stir.
+      5. Stud peach skins with cloves and add to bowl.
+      6. Add champagne.
+      7. Stir gently",
+      "image": '',
+      "description": "A bubbly concoction designed to lighten up any family
+      festivity."
+    },
+    {
+      "name": "Windex",
+      "ingredients": {
+        'vodka': '.75 ounces',
+        'triple sec': '.75 ounces',
+        'blue curacao': '.75 ounces',
+        "Rose's Lime Juice": '.75 ounces'
+      },
+      "instructions": "1. Fill a cocktail mixer with ice.
+      2. Add vodka, triple sec, blue curacao, and Rose’s
+      lime juice.
+      3. Shake.
+      4. Strain into a rocks glass.",
+      "image": '',
+      "description": "Whether drinking it or using it to clean windows, one firm
+      rule: no streaking."
+    },
+    { "name": "Will Rogers Cocktail",
+      "ingredients": {
+        'gin': '1.5 ounces',
+        'dry vermouth': '.5 ounces',
+        'orange juice': '.5 ounces',
+        'orange caracao': '.5 ounces'
+      },
+      "instructions": "1. Fill cocktail shaker with ice.
+      2. Add gin, vermouth, orange juice, and curacao.
+      3. Shake.
+      4. Strain into a cocktail glass.",
+      "image": '',
+      "description": "Will Rogers never met a man he didn’t like. Drink these,
+      hug the guy on the next bar stool."
+    },
+    { "name": "Wild Fling",
+      "ingredients": {
+        'wilderberry schnapps': '1.5 ounces',
+        'cranberry juice': 'splash',
+        'pineapple juice': '4 ounces'
+      },
+      "instructions": "1. Pour schnapps in a highball glass with ice.
+          2. Fill with pineapple juice.
+          3. Splash with cranberry juice.
+          4. Stir.",
+      "image": '',
+      "description": "Everybody should have at least one in their lifetime."
+    },
+    { "name": "Wiki Waki Woo",
+      "ingredients": {
+        'vodka': '.5 ounce',
+        'rum': '.5 ounce',
+        '151-proof rum': '.5 ounce',
+        'tequila': '.5 ounce',
+        'triple sec': '.5 ounce',
+        'amaretto': '1 ounce',
+        'orange juice': '1 ounce',
+        'pineapple juice': '1 ounce',
+        'cranberry juice': '1 ounce'
+      },
+      "instructions": "1. Pour all ingredients in a cocktail mixer filled with
+      ice.
+      2. Stir.
+      3. Pour into a hurricane glass.
+      4. Garnish with an orange slice and a cherry.",
+      "image": '',
+      "description": "Watch out for that wave, dude."
+    },
+    { "name": "Widow's Kiss",
+      "ingredients": {
+        'apple brandy': '1 ounce',
+        'yellow Chartreuse': '.75 ounce',
+        'Benedictine': '.75 ounce',
+        'Angostura bitters': 'dash'
+      },
+      "instructions": "1. Fill cocktail shaker with ice.
+      2. Add apple brandy, yellow Chartreuse, Benedictine,
+      and bitters.
+      3. Shake.
+      4. Strain into a cocktail glass.
+      5. Garnish with a strawberry",
+      "image": '',
+      "description": "As mysterious and alluring as its name"
+    },
+    { "name": "Widow's Dream",
+      "ingredients": {
+        'Benedictine': '2 ounces',
+        'heavy cream': '1 ounce',
+        'egg': '1',
+        'Angostura bitters': 'dash'
+      },
+      "instructions": "1. Fill cocktail shaker with ice.
+      2. Add Benedictine, heavy cream, and egg.
+      3. Shake.
+      4. Strain into a chilled cocktail glass.",
+      "image": '',
+      "description": "For those still in mourning, this makes for some solid
+      woolgathering."
+    },
+    { "name": "Wiz Bang",
+      "ingredients": {
+        'scotch': '2 ounces',
+        'dry vermouth': '.5 ounce',
+        'Pernod': '2 dashes',
+        'orange bitters': '2 dashes'
+      },
+      "instructions": "1. Fill cocktail shaker with ice.
+      2. Add scotch, vermouth, grenadine, Pernod, and
+      bitters.
+      3. Shake.
+      4. Strain into a chilled martini glass.",
+      "image": '',
+      "description": "Drink this, and you get a very pleasant whiz-bang feeling
+      bouncing around in your head. Do not be alarmed."
+    },
+    { "name": "Whiteout Martini for Two",
+      "ingredients": {
+        'vanilla vodka': '4 ounces',
+        'white crème de cacao': '1 ounce',
+        'white chocolate liqueur': '2 ounces'
+      },
+      "instructions": "1. Fill cocktail shaker with ice.
+      2. Add vodka.
+      3. Shake.
+      4. Add white crème de cacao and white chocolate
+      liqueur.
+      5. Swirl shaker for one minute.
+      6. Rest shaker for one minute.
+      7. Strain into two frozen martini glasses.
+      8. Dust with coconut flakes.",
+      "image": '',
+      "description": "No mistake here."
+    },
+    { "name": "White Russian",
+      "ingredients": {
+        'coffee liqueur': '1 ounce',
+        'vodka': '2 ounces',
+        'milk or cream': '4 ounces'
+      },
+      "instructions": "1. Fill cocktail shaker with ice.
+      2. Add vodka.
+      3. Shake.
+      4. Add white crème de cacao and white chocolate
+      liqueur.
+      5. Swirl shaker for one minute.
+      6. Rest shaker for one minute.
+      7. Strain into two frozen martini glasses.
+      8. Dust with coconut flakes.",
+      "image": '',
+      "description": "The perfect nightcap to sip in an icy, abandoned dacha."
+    },
+    { "name": "White Heart",
+      "ingredients": {
+        'sambuca': '.5 ounce',
+        'crème de cacao': '.5 ounce',
+        'cream': '2 ounces'
+      },
+      "instructions": "1. Fill cocktail shaker with ice.
+          2. Add sambuca, crème de cacao, and cream.
+          3. Shake.
+          4. Strain into a chilled stemmed glass.",
+      "image": '',
+      "description": "Mix this one up for the icy hearted date du jour, and see
+      your fortunes improve dramatically."
+    },
+    { "name": "White Grape, Tangerine, and Sparkling Wine Punch",
+      "ingredients": {
+        'unsweetened white grape juice': '48 ounces',
+        'frozen tangerine juice concentrate, thawed': '6 ounces',
+        'club soda': '8 ounces',
+        'brandy': '3 ounces',
+        'lemon juice': '2 ounces',
+        'sweet sparkling wine': '1/5',
+        'thin slices of tangerine': 'as desired'
+      },
+      "instructions": "1. Pour ingredients into a punch bowl over a block of
+          ice.
+          2. Stir.
+          3. Cover punch bowl and refrigerate until cold.
+          4. Add sparkling wine prior to serving.
+          5. Float tangerine slices.
+          (Serves 15–20)",
+          'image': '',
+          'description': 'Guests expecting the same old champagne punch? No way.
+          Zig and keep them guessing.'
+    },
+    { "name": "White Elephant",
+      "ingredients": {
+        'vodka': '.5 ounce',
+        'crème de cacao': '.5 ounce',
+        'cream': '2 ounces'
+      },
+      "instructions": "    1. Fill cocktail shaker with ice.
+          2. Add vodka, crème de cacao, and cream.
+          3. Shake.
+          4. Strain into a chilled cocktail glass.",
+          'image': '',
+          'description': 'Fun fact: In the story of Buddha, the white elephant is
+          associated with fertility and knowledge.'
+    },
+    { "name": "Whispering Reef",
+      "ingredients": {
+        'dry gin': '1 ounce',
+        'crème de menthe': '1 ounce',
+        'pineapple juice': '2 ounces'
+      },
+      "instructions": "1. Fill cocktail shaker with cracked ice.
+        2. Add gin, crème de menthe, and pineapple juice.
+        3. Shake.
+        4. Strain into a chilled cocktail glass.",
+      'image': '',
+      'description': 'Hey, Fred. Did that reef just whisper to me? No, Barney. That’s your conscience telling you to lay off the gin.'
+    },
+    { "name": "Whiskey Sour",
+      "ingredients": {
+        'whiskey': '1 ounce',
+        'sour mix': '2 ounces',
+        'pineapple juice': '2 ounces'
+      },
+      "instructions": "1. Fill a cocktail shaker with ice.
+        2. Add whiskey and sour mix.
+        3. Shake.
+        4. Strain into a sour glass.
+        5. Garnish with a cherry",
+      'image': '',
+      'description': 'The Popeye of drinks. It am what it am.'
     },
